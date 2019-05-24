08506658fb3195c4a294d6e0f5f26febc8f708d4
Arindam Das <arindam.das@codenation.co.in>
2019-05-24 09:49:57 +0530

    test3

---
+++ file1 (copy).java
@@ -0,0 +1,49 @@
+// Java program to illustrate 
+// implementing Ennead Class  
+// from Octet Class  
+// using direct values 
+  
+import java.util.*; 
+import org.javatuples.*; 
+  
+class GfG { 
+  
+    public static void main(String[] args) 
+    { 
+        // Create Octet 
+        Octet<String, String, String, String, String, String, String, String> 
+            octet = new Octet<String, String, String, String, String, String, String, String>( 
+                "Octet", "Septet", "Sextet", "Quintet", "Quartet", "Triplet", "Pair", "Unit"); 
+  
+        // Print the Octet 
+        System.out.println("Octet: " + octet); 
+  
+        // Create Ennead from Octet 
+        Ennead<String, String, String, String, String, String, String, String, String> 
+            ennead = new Ennead<String, String, String, String, String, String, String, String, String>( 
+                "Ennead", 
+                octet.getValue0(), 
+                octet.getValue1(), 
+                octet.getValue2(), 
+                octet.getValue3(), 
+                octet.getValue4(), 
+                octet.getValue5(), 
+                octet.getValue6(), 
+                octet.getValue7()); 
+
+	Ennead<String, String, String, String, String, String, String, String, String> 
+            ennead = new Ennead<String, String, String, String, String, String, String, String, String>( 
+                "Ennead", 
+                octet.getValue0(), 
+                octet.getValue1(), 
+                octet.getValue2(), 
+                octet.getValue3(), 
+                octet.getValue4(), 
+                octet.getValue5(), 
+                octet.getValue6(), 
+                octet.getValue7()); 
+  
+        // Print the Ennead 
+        System.out.println("Ennead: " + ennead); 
+    } 
+} 
